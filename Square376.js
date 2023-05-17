d4265b521426a2ac1a47b9b9d9e41ebc7435a3e7
0xBigBee <0xbigbee@protonmail.com>
2023-05-16 21:43:03 -0700

    refactor: add new files

---
+++ Square376.js
@@ -0,0 +1,64 @@
+let colors;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	colors = chromatoneColors();
+}
+
+function Square376A(x, y, span, pixel, bg, c1) {
+	const half = span/2;
+	const hp = pixel/2;
+	const sp = span - hp
+	push();
+	translate(x, y);
+	noStroke();
+	fill(c1);
+	rect(0, 0, span, span);
+	stroke(bg);
+	strokeWeight(pixel);
+	line(hp, 0, sp, 0);
+	line(0, hp, 0, sp);
+	const cut = (span-pixel) / 6;
+	noStroke();
+	rect(cut+hp, hp, cut, sp);
+	rect(hp, cut+hp, sp, cut);
+	fill(colors.background());
+	triangle(cut+hp, hp,
+					 sp-cut, hp,
+					 span/2, cut+hp);
+	triangle(cut+hp, sp,
+					 sp-cut, sp,
+					 span/2, sp-cut);
+	triangle(hp, cut+hp,
+					 hp, sp-cut,
+					 cut+hp, span/2);
+	triangle(sp, cut+hp,
+					 sp, sp-cut,
+					 sp-cut, span/2);
+	rect(cut+hp, cut+hp, cut, cut);
+	fill(colors.get(colors.index));
+	beginShape();
+	vertex(span/2, cut+hp);
+	vertex(sp-cut, span/2);
+	vertex(span/2, sp-cut);
+	vertex(cut+hp, span/2);
+	endShape(CLOSE);
+	pop();
+}
+
+function draw() {
+	const bg = colors.background();
+	const colorA = colors.next()
+	background(bg);
+	
+	const span = windowWidth / 10; 
+	strokeCap(SQUARE);
+	const pixel = Math.floor(span/7);
+	for (let y=0; y*span<windowHeight; y++) {
+		for (let x=0; x*span<windowWidth; x++) {
+			Square376A(x*span, y*span, span, pixel, bg, colorA);
+			fill(colors.next());
+		}
+	}
+	noLoop();
+}
--- color-stack.js
+++ color-stack.js
@@ -1,12 +1,17 @@
+// Language: javascript
+// Needs: https://unpkg.com/chromotome@1.19.1/dist/index.umd.js
 class RingStack {
   constructor(series, options = {}) {
     this.series = series;
     this.options = options;
-    this.index = 0;
+    this.index = -1;
   }
 
   burn(index = -1) {
-    const burnIx = index === -1 ? this.index : index % this.series.length;
+    let burnIx = index === -1 ? this.index : index % this.series.length;
+    if (burnIx === -1) {
+      burnIx = 0;
+    }
     const val = this.series[burnIx];
     const trimmed = [];
     this.series.forEach((c, ix) => {
@@ -28,6 +33,15 @@ class RingStack {
     return new RingStack(this.series);
   }
 
+  dict(names) {
+    const ret = {};
+    names.forEach((c, ix) => {
+      const key = names[ix];
+      ret[names[ix]] = key === "bg" ? this.background() : this.next();
+    });
+    return ret;
+  }
+
   get(index) {
     if (index === undefined) {
       return this.get(this.index);
@@ -35,14 +49,20 @@ class RingStack {
     return this.series[index % this.series.length];
   }
 
-  next() {
-    const val = this.series[this.index];
+  next(count = 1) {
     this.reset(this.index + 1);
-    return val;
+    if (count === 1) {
+      return this.get(this.index);
+    }
+    const ret = [];
+    for (let i = 0; i < count; i++) {
+      ret.push(this.get(this.index + i));
+    }
+    return ret;
   }
 
   random() {
-    const index = int(random(this.series.length));
+    const index = Math.floor(Math.random() * this.series.length);
     return this.series[index];
   }
 
@@ -78,38 +98,340 @@ class ColorStack extends RingStack {
 
   nextWithOpacity(opacity) {
     const val = this.next();
-    console.log(`val: ${val}`);
-    const r = val.slice(1, 3);
-    const g = val.slice(3, 5);
-    const b = val.slice(5, 7);
-    const rgba = `rgba(${unhex(r)}, ${unhex(g)}, ${unhex(b)}, ${opacity})`;
-    console.log({ rgba, r, g, b });
+    const [r, g, b] = [val.slice(1, 3), val.slice(3, 5), val.slice(5, 7)].map(unhex);
+    const rgba = `rgba(${r}, ${g}, ${b}, ${opacity})`;
     return color(rgba);
   }
 }
 
-function coolorsColors(url, interleave) {
-  const raw = url.split("/");
-  const colors = raw[raw.length - 1].split("-").map((c) => `#${c}`);
-  if (!interleave) {
-    return new ColorStack(colors);
+function chromatoneColors() {
+  const chroma = chromotome.get();
+  return new ColorStack(chroma.colors, chroma);
+}// Language: javascript
+// Needs: https://unpkg.com/chromotome@1.19.1/dist/index.umd.js
+class RingStack {
+  constructor(series, options = {}) {
+    this.series = series;
+    this.options = options;
+    this.index = -1;
   }
-  const wider = [];
-  colors.forEach((c) => {
-    wider.push(c);
-    wider.push(interleave);
-  });
-  return new ColorStack(wider);
+
+  burn(index = -1) {
+    let burnIx = index === -1 ? this.index : index % this.series.length;
+    if (burnIx === -1) {
+      burnIx = 0;
+    }
+    const val = this.series[burnIx];
+    const trimmed = [];
+    this.series.forEach((c, ix) => {
+      if (ix !== burnIx) {
+        trimmed.push(c);
+      }
+    });
+    this.series = trimmed;
+    this.reset(index);
+    return val;
+  }
+
+  burnRandom() {
+    const index = Math.floor(Math.random() * this.series.length);
+    return this.burn(index);
+  }
+
+  duplicate() {
+    return new RingStack(this.series);
+  }
+
+  dict(names) {
+    const ret = {};
+    names.forEach((c, ix) => {
+      const key = names[ix];
+      ret[names[ix]] = key === "bg" ? this.background() : this.next();
+    });
+    return ret;
+  }
+
+  get(index) {
+    if (index === undefined) {
+      return this.get(this.index);
+    }
+    return this.series[index % this.series.length];
+  }
+
+  next(count = 1) {
+    this.reset(this.index + 1);
+    if (count === 1) {
+      return this.get(this.index);
+    }
+    const ret = [];
+    for (let i = 0; i < count; i++) {
+      ret.push(this.get(this.index + i));
+    }
+    return ret;
+  }
+
+  random() {
+    const index = Math.floor(Math.random() * this.series.length);
+    return this.series[index];
+  }
+
+  reset(ix = 0) {
+    this.index = ix % this.series.length;
+    return this;
+  }
+
+  shuffle() {
+    this.series = shuffle(this.series);
+    return this;
+  }
+}
+
+class ColorStack extends RingStack {
+  duplicate() {
+    return new ColorStack(this.series);
+  }
+
+  background() {
+    if (!this.options.background) {
+      this.options.background = this.burnRandom();
+    }
+    return this.options.background;
+  }
+
+  stroke() {
+    if (!this.options.stroke) {
+      this.options.stroke = this.burnRandom();
+    }
+    return this.options.stroke;
+  }
+
+  nextWithOpacity(opacity) {
+    const val = this.next();
+    const [r, g, b] = [val.slice(1, 3), val.slice(3, 5), val.slice(5, 7)].map(unhex);
+    const rgba = `rgba(${r}, ${g}, ${b}, ${opacity})`;
+    return color(rgba);
+  }
+}
+
+function chromatoneColors() {
+  const chroma = chromotome.get();
+  return new ColorStack(chroma.colors, chroma);
+}// Language: javascript
+// Needs: https://unpkg.com/chromotome@1.19.1/dist/index.umd.js
+class RingStack {
+  constructor(series, options = {}) {
+    this.series = series;
+    this.options = options;
+    this.index = -1;
+  }
+
+  burn(index = -1) {
+    let burnIx = index === -1 ? this.index : index % this.series.length;
+    if (burnIx === -1) {
+      burnIx = 0;
+    }
+    const val = this.series[burnIx];
+    const trimmed = [];
+    this.series.forEach((c, ix) => {
+      if (ix !== burnIx) {
+        trimmed.push(c);
+      }
+    });
+    this.series = trimmed;
+    this.reset(index);
+    return val;
+  }
+
+  burnRandom() {
+    const index = Math.floor(Math.random() * this.series.length);
+    return this.burn(index);
+  }
+
+  duplicate() {
+    return new RingStack(this.series);
+  }
+
+  dict(names) {
+    const ret = {};
+    names.forEach((c, ix) => {
+      const key = names[ix];
+      ret[names[ix]] = key === "bg" ? this.background() : this.next();
+    });
+    return ret;
+  }
+
+  get(index) {
+    if (index === undefined) {
+      return this.get(this.index);
+    }
+    return this.series[index % this.series.length];
+  }
+
+  next(count = 1) {
+    this.reset(this.index + 1);
+    if (count === 1) {
+      return this.get(this.index);
+    }
+    const ret = [];
+    for (let i = 0; i < count; i++) {
+      ret.push(this.get(this.index + i));
+    }
+    return ret;
+  }
+
+  random() {
+    const index = Math.floor(Math.random() * this.series.length);
+    return this.series[index];
+  }
+
+  reset(ix = 0) {
+    this.index = ix % this.series.length;
+    return this;
+  }
+
+  shuffle() {
+    this.series = shuffle(this.series);
+    return this;
+  }
+}
+
+class ColorStack extends RingStack {
+  duplicate() {
+    return new ColorStack(this.series);
+  }
+
+  background() {
+    if (!this.options.background) {
+      this.options.background = this.burnRandom();
+    }
+    return this.options.background;
+  }
+
+  stroke() {
+    if (!this.options.stroke) {
+      this.options.stroke = this.burnRandom();
+    }
+    return this.options.stroke;
+  }
+
+  nextWithOpacity(opacity) {
+    const val = this.next();
+    const [r, g, b] = [val.slice(1, 3), val.slice(3, 5), val.slice(5, 7)].map(unhex);
+    const rgba = `rgba(${r}, ${g}, ${b}, ${opacity})`;
+    return color(rgba);
+  }
+}
+
+function chromatoneColors() {
+  const chroma = chromotome.get();
+  return new ColorStack(chroma.colors, chroma);
+}// Language: javascript
+// Needs: https://unpkg.com/chromotome@1.19.1/dist/index.umd.js
+class RingStack {
+  constructor(series, options = {}) {
+    this.series = series;
+    this.options = options;
+    this.index = -1;
+  }
+
+  burn(index = -1) {
+    let burnIx = index === -1 ? this.index : index % this.series.length;
+    if (burnIx === -1) {
+      burnIx = 0;
+    }
+    const val = this.series[burnIx];
+    const trimmed = [];
+    this.series.forEach((c, ix) => {
+      if (ix !== burnIx) {
+        trimmed.push(c);
+      }
+    });
+    this.series = trimmed;
+    this.reset(index);
+    return val;
+  }
+
+  burnRandom() {
+    const index = Math.floor(Math.random() * this.series.length);
+    return this.burn(index);
+  }
+
+  duplicate() {
+    return new RingStack(this.series);
+  }
+
+  dict(names) {
+    const ret = {};
+    names.forEach((c, ix) => {
+      const key = names[ix];
+      ret[names[ix]] = key === "bg" ? this.background() : this.next();
+    });
+    return ret;
+  }
+
+  get(index) {
+    if (index === undefined) {
+      return this.get(this.index);
+    }
+    return this.series[index % this.series.length];
+  }
+
+  next(count = 1) {
+    this.reset(this.index + 1);
+    if (count === 1) {
+      return this.get(this.index);
+    }
+    const ret = [];
+    for (let i = 0; i < count; i++) {
+      ret.push(this.get(this.index + i));
+    }
+    return ret;
+  }
+
+  random() {
+    const index = Math.floor(Math.random() * this.series.length);
+    return this.series[index];
+  }
+
+  reset(ix = 0) {
+    this.index = ix % this.series.length;
+    return this;
+  }
+
+  shuffle() {
+    this.series = shuffle(this.series);
+    return this;
+  }
+}
+
+class ColorStack extends RingStack {
+  duplicate() {
+    return new ColorStack(this.series);
+  }
+
+  background() {
+    if (!this.options.background) {
+      this.options.background = this.burnRandom();
+    }
+    return this.options.background;
+  }
+
+  stroke() {
+    if (!this.options.stroke) {
+      this.options.stroke = this.burnRandom();
+    }
+    return this.options.stroke;
+  }
+
+  nextWithOpacity(opacity) {
+    const val = this.next();
+    const [r, g, b] = [val.slice(1, 3), val.slice(3, 5), val.slice(5, 7)].map(unhex);
+    const rgba = `rgba(${r}, ${g}, ${b}, ${opacity})`;
+    return color(rgba);
+  }
 }
 
 function chromatoneColors() {
   const chroma = chromotome.get();
   return new ColorStack(chroma.colors, chroma);
 }
-
-module.exports = {
-  coolorsColors,
-  chromatoneColors,
-  ColorStack,
-  RingStack,
-};
